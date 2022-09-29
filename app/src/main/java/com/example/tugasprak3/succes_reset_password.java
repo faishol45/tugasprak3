1a68e4c2af099ed53b1258f0a5034165ca4365c5
faishol45 <dhiaputrawm@gmail.com>
2022-09-29 11:21:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasprak3/succes_reset_password.java
@@ -0,0 +1,14 @@
+package com.example.tugasprak3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class succes_reset_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succes_reset_password);
+    }
+}
